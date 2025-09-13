f7e5f2bbfade6e7f2ed5d1ec0cd17c54492ff206
Timitunes <timiswift0@gmail,com>
2025-09-13 20:52:04 +0100

    My first commit

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+//import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "../components/Provider";
+
+/* const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+}); */
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Emmanuel's Portfolio",
+  description: "Moderm & Minimalist Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={inter.className}
+      >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          
+          >
+            {children}
+          </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+   <main className="bg-black-100 flex flex-col justify-center items-center w-full overflow-hidden mx-auto sm:px-10 px-5">
+    <div className="max-w-7xl w-full">
+      <Hero />
+    </div>
+   </main>
+  );
+}
